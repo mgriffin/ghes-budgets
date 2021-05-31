6724d4ff2c36ae2886b6538acb9f69bed4f9e180
Mike Griffin <mgriffin@github.com>
2021-05-31 16:59:11 +0100

    Makes the javascript changes

--- js/budgets.js
+++ js/budgets.js
@@ -1,4 +1,14 @@
-function github_unicorns(memory) {
+let budget = {};
+
+budget.github_unicorns = function(ghes_version, memory) {
+  if (ghes_version == '2.20') {
+    return github_unicorns_2_20(memory);
+  } else if (ghes_version == '2.22') {
+    return github_unicorns_2_22(memory);
+  }
+}
+
+github_unicorns_2_20 = function(memory) {
   var ghuni_budget = memory * 0.25;
   if (ghuni_budget > 18) {
     return 30;
@@ -15,7 +25,32 @@ function github_unicorns(memory) {
   return 4;
 }
 
-function github_ernicorns(memory) {
+github_unicorns_2_22 = function(memory) {
+  var ghuni_budget = memory * 0.25;
+  if (ghuni_budget >= 18) {
+    return 30;
+  }
+  if (ghuni_budget >= 16) {
+    return 24;
+  }
+  if (ghuni_budget >= 8) {
+    return 16;
+  }
+  if (ghuni_budget >= 4) {
+    return 8;
+  }
+  return 4;
+}
+
+budget.github_ernicorns = function(ghes_version, memory) {
+  if (ghes_version == '2.20') {
+    return github_ernicorns_2_20(memory);
+  } else if (ghes_version == '2.22') {
+    return github_ernicorns_2_22(memory);
+  }
+}
+
+github_ernicorns_2_20 = function(memory) {
   var gherni_budget = memory * 0.10;
   if (gherni_budget > 15.4) {
     return 30;
@@ -35,7 +70,35 @@ function github_ernicorns(memory) {
   return 2;
 }
 
-function gitauth_unicorns(memory) {
+github_ernicorns_2_22 = function(memory) {
+  var gherni_budget = memory * 0.10;
+  if (gherni_budget >= 15.4) {
+    return 30;
+  }
+  if (gherni_budget >= 7.2) {
+    return 15;
+  }
+  if (gherni_budget >= 6.4) {
+    return 12;
+  }
+  if (gherni_budget >= 3.2) {
+    return 8;
+  }
+  if (gherni_budget >= 1.6) {
+    return 4;
+  }
+  return 2;
+}
+
+budget.gitauth_unicorns = function(ghes_version, memory) {
+  if (ghes_version == '2.20') {
+    return gitauth_unicorns_2_20(memory);
+  } else if (ghes_version == '2.22') {
+    return gitauth_unicorns_2_22(memory);
+  }
+}
+
+gitauth_unicorns_2_20 = function(memory) {
   var gauni_budget = memory * 0.25;
   if (gauni_budget > 16) {
     return 6;
@@ -46,7 +109,18 @@ function gitauth_unicorns(memory) {
   return 2;
 }
 
-function total_workers(cpu) {
+gitauth_unicorns_2_22 = function(memory) {
+  var gauni_budget = memory * 0.25;
+  if (gauni_budget >= 16) {
+    return 6;
+  }
+  if (gauni_budget >= 6) {
+    return 4;
+  }
+  return 2;
+}
+
+total_workers = function(cpu) {
   var total_workers = Math.floor(cpu * 1.5);
   if (total_workers > 18) {
     return 18;
@@ -54,18 +128,4 @@ function total_workers(cpu) {
   return total_workers;
 }
 
-document.getElementById('memory').addEventListener('input', function() {
-  if (this.value < 16) {
-    this.value = 16;
-  }
-  document.getElementById('githubunicorns').innerHTML = github_unicorns(this.value);
-  document.getElementById('githubernicorns').innerHTML = github_ernicorns(this.value);
-  document.getElementById('gitauthunicorns').innerHTML = gitauth_unicorns(this.value);
-});
-
-document.getElementById('cpus').addEventListener('change', function() {
-  if (this.value < 2) {
-    this.value = 2;
-  }
-  document.getElementById('highworkers').innerHTML = total_workers(this.value) - 2;
-});
+module.exports = budget;
